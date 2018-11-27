cade97bb8ee8d68347f5f3f668d861413b240fca
angeloLuna <angel.luna.najera@gmail.com>
2018-11-26 21:43:32 -0600

    bootstrap css y js

---
+++ jsbasico/main.js
@@ -0,0 +1,65 @@
+// modo estricto de JS
+'use strict'
+// // let cuadrado = 0;
+// // Input desde el anvegador
+// let numero = prompt('Ingresa un número');
+
+// numero = numero * numero;
+
+// // Output en la connsola del navegador
+// console.log(numero);
+
+// let numero = prompt('ingresa un número');
+
+// numero = numero * 1
+
+// if (numero) {
+//   console.log(numero * numero);
+// } else{
+//   console.log('Ingresa un número')
+  
+// }
+
+
+// Preguntar al usuario: nombre, primer apellido, segundo apellido; e imprimir en una sola linea los tres. (concatenación);
+
+// pedir al usuario una contraseña y validar que sea igual a 'gatitos13'
+
+// Pedir un número, imprimir la mitad del numero original, imprimir el doble del numero original, imprimir el cuadrado del numero original, imprimir el valor original entre 3
+
+// let nombre = prompt('nombre')
+// let apellido = prompt('apellido')
+// let apellido2 = prompt('apellido2')
+
+// console.log(nombre + " " + apellido + " " + apellido2)
+
+// let pass = prompt('contraseña');
+
+// if (pass === 'gatitos13' ){
+//   console.log('Conntraseña correcta');
+
+// }else{
+//   console.log('Conntraseña incorrecta');
+// }
+
+let numero = prompt('número');
+
+let mitad = numero / 2;
+let doble = numero * 2;
+let cuadrado = numero * numero;
+let tercio = numero / 3;
+
+console.log(mitad)
+console.log(doble)
+console.log(cuadrado)
+console.log(tercio)
+
+
+
+
+
+
+
+
+
+
