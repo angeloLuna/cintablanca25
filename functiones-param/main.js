9ae5c8e3c455f75aa164a6c00be2b88dcd7c993d
angeloLuna <angel.luna.najera@gmail.com>
2018-12-05 21:43:41 -0600

    repaso rapido

--- functiones-param/main.js
+++ functiones-param/main.js
@@ -26,13 +26,39 @@
 // // console.log( multiplicacion(9, 2) );
 // // console.log( multiplicacion(2, 2) );
 
-const boton =  document.getElementById('boton');
-const titulo = document.getElementById('titulo');
-const nombre = document.getElementById('nombre');
+// const boton =  document.getElementById('boton');
+// const titulo = document.getElementById('titulo');
+// const nombre = document.getElementById('nombre');
 
-const handleClick = () => {
-  titulo.innerHTML = nombre.value 
-}
+// const handleClick = () => {
+//   titulo.innerHTML = nombre.value 
+// }
 
-boton.addEventListener('click', handleClick)
+// boton.addEventListener('click', handleClick)
+
+// Vinculamos elementos de HTML con JS a través de su id
+// const boton = document.getElementById('boton');
+// const palabra1 = document.getElementById('palabra1');
+// const palabra2 = document.getElementById('palabra2');
+// const resultado1 = document.getElementById('resultado1')
+// const resultado2 = document.getElementById('resultado2');
+
+
+
+// // Funcion que se ejecuta al hacer click en el boton
+// const handleClick = () =>{
+
+//   let mayus =  palabra1.value.toUpperCase();
+//   let minus = palabra2.value.toLowerCase();
+
+//   resultado1.innerHTML = mayus;
+//   resultado2.innerHTML = minus;
+// }
+
+
+
+
+
+// // Agregamos escuchador de evento 'click'
+// boton.addEventListener('click', handleClick)
 
---
+++ repaso/main.js
@@ -0,0 +1,21 @@
+// let array = [4, 6, 9, "hola"]
+
+
+
+// for(let i = 0; i < array.length  ; i++ ){
+
+//   console.log(  array[i]     )
+
+// }
+let edad = 19;
+
+if ( edad > 17 ) {
+  console.log("eres mayor de edad");
+} else if (edad >12) {
+  console.log("tienes más de doce, pero menos de 18");
+} else {
+  console.log("eres menor a 12 años")
+}
+
+
+
